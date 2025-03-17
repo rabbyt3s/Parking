5b28c9c0cd09b44889cb84f6112d1078cae3e4ba
Rabbyt3s <0byspy0@gmail.com>
2025-03-17 11:26:14 +0100

    Ajout des middlewares

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check() || !auth()->user()->estAdmin()) {
+            abort(403, 'Accès non autorisé.');
+        }
+
+        return $next($request);
+    }
+}
